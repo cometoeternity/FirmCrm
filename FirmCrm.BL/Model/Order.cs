fdcf3edd2e93601250b886c5d2d10711f8fd177f
dotoeternity@outlook.com <dotoeternity@outlook.com>
2020-02-04 21:50:55 +0700

    Создание сущностей системы. Подготовка в внедрению Entity Framework.

---
+++ FirmCrm.BL/Model/Order.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirmCrm.BL.Model
+{
+    public class Order
+    {
+        public int OrderId { get; set; }
+        public int CustomerId { get; set; }
+        public virtual Customers Customers { get; set; }
+        public DateTime Created { get; set; }
+        public int EmployeeId { get; set; }
+        public virtual Employee Employee { get; set;}
+        public ICollection<Ordered> Ordereds { get; set; }
+        
+    }
+}
