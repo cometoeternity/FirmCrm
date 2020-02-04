de5f2a270538bf4746358d9e7cf894e6196b84d5
dotoeternity@outlook.com <dotoeternity@outlook.com>
2020-02-04 21:50:55 +0700

    Создание сущностей системы. Подготовка в внедрению Entity Framework.

---
+++ FirmCrm.BL/Model/Customers.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirmCrm.BL.Model
+{
+    public class Customers
+    {
+        public int CustomersId { get; set; }
+        public string CustomersName { get; set; }
+        public int PhoneNumber { get; set; }
+        public ICollection<Order> Orders { get; set; }
+        public override string ToString()
+        {
+            return $"ID заказчика - {CustomersId} Название фирмы заказчика - {CustomersName}";
+        }
+    }
+}
---
+++ FirmCrm.BL/Model/Employee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirmCrm.BL.Model
+{
+    public class Employee
+    {
+        public int EmployeeId { get; set; }
+        public string Surname { get; set; }
+        public string Name { get; set; }
+        public ICollection<Order> Orders { get; set; }
+        public override string ToString()
+        {
+            return $"ID сотрудника - {EmployeeId}, Фамилия и Имя сотрудника - {Surname} {Name} ";
+        }
+    }
+}
